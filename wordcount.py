e4aa9c4890131bd670cbd86e21e6c2a23be67f96
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 17:54:12 -0800

    Sort inside the sort

--- wordcount.py
+++ wordcount.py
@@ -13,12 +13,23 @@ for line in input_file:
         word_dict[each_word] = count
 
 # for each_word, count in sorted(word_dict.iteritems(), key = count):
-mini_dict = {}
-for each_word, count in sorted(word_dict.iteritems(), key = lambda x: x[1], reverse = True):
-    mini_dict[count].append(each_word)
- 
+# mini_dict = {}
+# for each_word, count in sorted(word_dict.iteritems(), key = lambda x: (x[1], x[0]), reverse = True):
+# # for each_word, count in sorted(word_dict.iteritems(), key = lambda x: x[1], reverse = True):
+#     # mini_dict[count].append(each_word)
 
+#     print  each_word, count 
 
-    print  each_word, count 
+new_dict = {}
+new_set = set(word_dict.values()) 
 
+for item in new_set:
+    new_list = sorted([word for word, count in word_dict.items() if count == item])
+    new_dict[item] = new_list
+    
+for key, list_of_values in sorted(new_dict.iteritems(), reverse = True):
+    for value in list_of_values:
+        print key, value
+
+    #print each_word, count
 
